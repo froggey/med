26a03501ee148effed806d76a240b91758960346
Henry Harrington <henry.harrington@gmail.com>
2015-04-20 22:45:40 +0100

    Improve find-defintion a bit.
    Make it work on macros and use FUNCTION-DEBUG-INFO instead of grovelling
    around in the constant pools of functions.

--- commands/find-definition.lisp
+++ commands/find-definition.lisp
@@ -3,28 +3,30 @@
 (defvar *mark-stack* ())
 
 (defun function-source-file (function-symbol)
-  (let ((string (sixth (sys.int::function-pool-object
-                         (symbol-function function-symbol) 1))))
+  (let* ((function (or (macro-function function-symbol)
+                       (fdefinition function-symbol)))
+         (string (sixth (sys.int::function-debug-info function))))
     (when string
       (if (eql (char string 0) #\#)
         (read-from-string string) ; convert pathname
         (pathname string)))))
 
 (defun function-top-level-form-number (function-symbol)
-   (seventh (sys.int::function-pool-object (symbol-function function-symbol) 1)))
+  (let ((function (or (macro-function function-symbol)
+                      (fdefinition function-symbol))))
+    (seventh (sys.int::function-debug-info function))))
 
 (defun find-definition (function-symbol)
-  (let* ((buffer (current-buffer *editor*)))
-    (let ((file (function-source-file function-symbol))
-          (form (function-top-level-form-number function-symbol)))
-      (if (and file form) 
-        (progn
-          (format t "~A ~A ~A ~A~%" buffer *package* file form)
-          (let ((buffer (find-file file)))
-            (move-beginning-of-buffer buffer)
-            (move-sexp buffer (1+ form))
-            (move-sexp buffer -1))
-        (format t "Cannot find definition for function ~A" function-symbol))))))
+  (let* ((buffer (current-buffer *editor*))
+         (file (function-source-file function-symbol))
+         (form (function-top-level-form-number function-symbol)))
+      (cond ((and file form)
+             (format t "~A ~A ~A ~A~%" buffer *package* file form)
+             (let ((buffer (find-file file)))
+               (move-beginning-of-buffer buffer)
+               (move-sexp buffer (1+ form))
+               (move-sexp buffer -1)))
+            (t (format t "Cannot find definition for function ~A" function-symbol)))))
 
 (defun find-definition-command ()
   (find-definition (read-from-string (symbol-at-point (current-buffer *editor*)))))
